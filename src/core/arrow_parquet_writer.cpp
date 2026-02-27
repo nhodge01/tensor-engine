9ebf3ddba22d22c1042135522cc09d4a69c794d8
Nathan Hodges <hodgesnathan@pm.me>
2026-02-27 21:15:33 +0000

    remove unused files

--- src/core/arrow_parquet_writer.cpp
+++
@@ -1,9 +0,0 @@
-#include "acrelab-gauss/arrow_parquet_writer.hpp"
-#include "batch_job.hpp"
-
-namespace acrelab {
-
-// Explicit template instantiation for BatchJob
-template void ArrowParquetWriter::WriteBatchJob<BatchJob>(const BatchJob* job);
-
-} // namespace acrelab
